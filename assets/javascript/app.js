9f3df9467b8f0e96d20c54e95ddc8a091361e06c
PLS <plsenh@gmail.com>
2019-01-30 05:41:57 -0500

    css

---
+++ assets/javascript/app.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+    var didScroll;
+
+    var lastScrollTop = 0;
+    var delta = 5;
+    var navbarHeight = $("header").outerHeight();
+
+    // on scroll, let the interval function know the user has scrolled
+    $(window).scroll(function (event) {
+        didScroll = true;
+    });
+    // run hasScrolled() and reset didScroll status
+    setInterval(function () {
+        if (didScroll) {
+            hasScrolled();
+            didScroll = false;
+        }
+    }, 250);
+
+    function hasScrolled() {
+        var st = $(this).scrollTop();
+
+        if (Math.abs(lastScrollTop - st) <= delta)
+            return;
+
+        // If current position > last position AND scrolled past navbar...
+        if (st > lastScrollTop && st > navbarHeight) {
+            // Scroll Down
+            $("header").removeClass("nav-down").addClass("nav-up");
+        } else {
+            // Scroll Up
+            // If did not scroll past the document (possible on mac)...
+            if (st + $(window).height() < $(document).height()) {
+                $("header").removeClass("nav-up").addClass("nav-down");
+            }
+        }
+
+        lastScrollTop = st;
+    }
+});
